9cfb91191c05ac8e7ba471317281ad629648623b
Maksym Dukhan <maxduh747@icloud.com>
2025-04-07 12:36:32 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const COST_PER_DAY = 40;
+  const DISCOUNT_FOR_3_DAYS = 20;
+  const DISCOUNT_FOR_7_DAYS = 50;
+  const MIN_DAYS_FOR_SMALL_DISCOUNT = 3;
+  const MIN_DAYS_FOR_LARGE_DISCOUNT = 7;
+
+  let total = days * COST_PER_DAY;
+
+  if (days >= MIN_DAYS_FOR_SMALL_DISCOUNT) {
+    total -= DISCOUNT_FOR_3_DAYS;
+  }
+
+  if (days >= MIN_DAYS_FOR_LARGE_DISCOUNT) {
+    total -= DISCOUNT_FOR_7_DAYS - DISCOUNT_FOR_3_DAYS;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
